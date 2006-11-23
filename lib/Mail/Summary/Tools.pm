ffab6aa08a8724006e41f6a9a1c10cef9f1cde49
Yuval Kogman <nothingmuch@woobling.org>
2006-11-23 14:33:04 +0000

    pod formatting

--- lib/Mail/Summary/Tools.pm
+++ lib/Mail/Summary/Tools.pm
@@ -237,8 +237,8 @@ L<Mail::Box>, L<App::Cmd>, L<Template>, L<Proc::InvokeEditor>, L<YAML>, L<YAML::
 =head1 VERSION CONTROL
 
 This module is maintained using Darcs. You can get the latest version from
-L<http://nothingmuch.woobling.org/Mail-Summary-Tools/>, and use `darcs send` to
-commit changes.
+L<http://nothingmuch.woobling.org/Mail-Summary-Tools/>, and use C<darcs send>
+to commit changes.
 
 =head1 AUTHORS
 
